55f9c791d055814665d1c2eda8c23898def696bf
Pradeep Sekar <pradeep87sekar@gmail.com>
2023-10-01 19:30:47 +1100

    Create / Integrate the Projects page
    - Updated `getProjectsData()` to cache and re-validate data every 5 minutes.

--- app/data.tsx
+++ app/data.tsx
@@ -6,11 +6,13 @@ export async function getProjectsData() {
     throw new Error("Unable to retrieve valid data URL for projects.");
   }
 
-  /**
-   * To accurately demonstrate the load time / behaviour of this sample app,
-   * we're manually opting out of the default ('force-cache') data caching.
-   */
-  const response = await fetch(API_DATAURL_PROJECTS, { cache: "no-store" });
+  const response = await fetch(API_DATAURL_PROJECTS, {
+    // Data can be fetched at build or request time, cached, and reused.
+    cache: "force-cache",
+
+    // Purge the data cache and re-fetch the latest data every 5 minutes.
+    next: { revalidate: 300 },
+  });
 
   if (!response.ok) {
     throw new Error("Failed to fetch data from the API for projects.");
