9e0aec908576f8e8d7486ac26eec74be658ac1fe
Pradeep Sekar <pradeep87sekar@gmail.com>
2023-12-18 00:32:54 +1100

    Updates to Projects page and data
    - Added `getTotalSizeOfAreas()` function to help calculate the total size of given areas (in hectares).
    
    - Added some common utility helper functions to deal with number type checks and rounding off decimals.

--- utils/managementAreas.ts
+++ utils/managementAreas.ts
@@ -1,4 +1,5 @@
 import { ProjectManagementArea } from "@/context/DataContext/types";
+import { isNumber } from "./common";
 
 export function getCenterOfAreas(areas?: ProjectManagementArea[]) {
   if (!Array.isArray(areas) || !areas.length) {
@@ -11,19 +12,19 @@ export function getCenterOfAreas(areas?: ProjectManagementArea[]) {
      * but until then, just use the first coordinate in array, and zoom out.
      */
     const areaCoordinates = areas[0]?.geometry?.coordinates?.[0]?.[0];
-    let longitude = areaCoordinates?.[0];
     let latitude = areaCoordinates?.[1];
+    let longitude = areaCoordinates?.[0];
 
     if (longitude && Array.isArray(longitude)) {
-      latitude = parseFloat((longitude[1] || longitude[0])?.toString());
-      longitude = parseFloat(longitude[0]?.toString());
+      latitude = parseFloat(`${longitude[1]}`);
+      longitude = parseFloat(`${longitude[0]}`);
     }
 
-    if (isNaN(longitude as number)) {
+    if (!isNumber(longitude)) {
       longitude = 0;
     }
 
-    if (isNaN(latitude as number)) {
+    if (!isNumber(latitude)) {
       latitude = 0;
     }
 
@@ -37,6 +38,23 @@ export function getCenterOfAreas(areas?: ProjectManagementArea[]) {
       longitude: 0,
       latitude: 0,
     };
+  }
+}
+
+export function getTotalSizeOfAreas(areas?: ProjectManagementArea[]) {
+  if (!Array.isArray(areas) || !areas.length) {
+    return 0;
+  }
+
+  try {
+    return areas.reduce((currTotal, area) => {
+      const { Area_ha = 0, area_ha = 0 } = area?.properties || {};
+      const size = parseFloat(`${Area_ha || area_ha}`);
+      return currTotal + (isNumber(size) ? size : 0);
+    }, 0);
+  } catch (error: unknown | any) {
+    console.log(error?.message);
+    return 0;
   }
 }
 
